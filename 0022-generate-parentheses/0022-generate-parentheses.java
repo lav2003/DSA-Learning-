d2ccd1ad5845144e43aeff64f3d9f7fd38c857e0
Lav Singh <lav.singh.0987@gmail.com>
2026-09-04 13:59:35 +0530

    Time: 1 ms (85.63%), Space: 44 MB (98.29%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+
+        fun(0, 0, n, temp, res);
+        return res;
+    }
+    public void fun(int open, int close, int n,StringBuilder temp, List<String> res) {
+        if (open == n && close == n) {
+            res.add(temp.toString());
+            return;
+        }
+        if (open < n) {
+            temp.append('(');
+            fun(open + 1, close, n, temp, res);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+        if (close < open) {
+            temp.append(')');
+            fun(open, close + 1, n, temp, res);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+}
