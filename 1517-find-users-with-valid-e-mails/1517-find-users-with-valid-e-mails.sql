5c895502a0284d309eb7fad45b36e909a6a7a550
Lav Singh <lav.singh.0987@gmail.com>
2026-09-11 15:27:33 +0530

    Time: 965 ms (26.17%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,7 +1,10 @@
 SELECT *
 FROM Users
-WHERE REGEXP_LIKE(
-    mail,
-    '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$',
-    'c'
-);
+WHERE REGEXP_LIKE(mail,'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$','c');
+
+
+
+-- SELECT *
+-- FROM Users
+-- WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
+--   AND mail NOT REGEXP '@leetcode[.]COM$';
