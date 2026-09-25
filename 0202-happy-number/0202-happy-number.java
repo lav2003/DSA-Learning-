20db7be0ddb8e0da5fa4ef07755d68d0dadbb356
Lav Singh <lav.singh.0987@gmail.com>
2026-09-26 00:04:28 +0530

    Time: 2 ms (13.98%), Space: 42 MB (94.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int fun(int n){
-        int sum = 0;
-        while(n>0){
-            int d = n%10;
-            n = n/10;
-            sum += d*d;
-        }
-        return sum;
-    }
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-        while(fast !=1){
-            slow = fun(slow);
-            fast = fun(fast);
-            fast = fun(fast);
-            if(fast == slow && slow!=1){
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        while (n != 1) {
+            if (mp.containsKey(n)) {
                 return false;
             }
+            mp.put(n, 1);
+            int sum = 0;
+            while (n > 0) {
+                int d = n % 10;
+                n = n / 10;
+                sum = sum + d * d;
+            }
+            n = sum;
         }
         return true;
-        
-        
     }
 }
